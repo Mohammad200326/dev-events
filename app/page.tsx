532e1ce90f8f924a7ad82b30892d4ec7159daf9a
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2025-11-06 00:55:43 +0200

    feat: create home page with dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/data/event.data";
+
 const Page = () => {
   return (
     <section>
@@ -7,6 +11,20 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
       </p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
